744c2496c9d6013a60c2c148f12c02207f57f84e
glider0 <fengyuntianlc@outlook.com>
2019-10-16 13:27:55 +0800

    recovery: port ranga-recovery-instruct to windows

--- create-windows-tarball.sh
+++ create-windows-tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp ranga-cli ranga-client-windows/usr/bin
+cp ranga-cli ranga-recovery-instruct  ranga-client-windows/usr/bin
 cp bash-completion/ranga-cli-completion.bash ranga-client-windows/usr/share/bash-completion/completions/ranga-cli
 
 7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=1024m -ms=on ranga-client-windows.7z ranga-client-windows/
